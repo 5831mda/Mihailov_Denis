d021669f70a9ea4d28a8f6d7a7c8d5daf6f0ff2c
5831mda <denchik_mih@mail.ru>
2017-02-26 19:55:12 +0700

    Добавил [TestCase] для класса Thesis()

--- IzdanieSourceTree1/ThesisTest/ThesisTest.cs
+++ IzdanieSourceTree1/ThesisTest/ThesisTest.cs
@@ -7,10 +7,16 @@ namespace ThesisTest
     public class TestThesis
     {
         [Test]
-        public void TestThesisInform()
+        [TestCase("Andreevich", "Mikhailov", "D.A.", "Bachelor", "Kemerovo", 234, 334, true,
+           "Mikhailov Andreevich : Bachelor / Mikhailov D.A. Andreevich - Kemerovo, 334. - 234c.",
+            TestName = "правильная строка")]
+        [TestCase("Andreevich", "Mikhailov", "D.A.", "Bachelor", "Kemerovo", 234, 334, false,
+           "Mikhailov Andreevich:BachelorMikhailovD.A. Andreevich - Kemerovo, 334. - 234c.",
+            TestName = "неправильная строка")]
+        public void TestThesisInform(string patronymic, string surname, string initials, string position,
+            string city, int year, int number, bool shouldEquals, string inform)
         {
-
-            /*var testThesis = new Thesis();
+            var testThesis = new Thesis();
             testThesis.Patronymic = "Andreevich";
             testThesis.Surname = "Mikhailov";
             testThesis.Initials = "D.A.";
@@ -18,12 +24,14 @@ namespace ThesisTest
             testThesis.City = "Kemerovo";
             testThesis.Year = 234;
             testThesis.Number = 334;
-
-            Assert.AreEqual("Mikhailov Andreevich : Bachelor / Mikhailov D.A. Andreevich - Kemerovo, 334. - 234c.",
-                testThesis.Inform);
-            Assert.AreNotEqual("Mikhailov Andreevich  BachelorMikhailov D. Andreevich - Kemerovo, 334. - 234c.",
-                testThesis.Inform);
-                */
+            if (shouldEquals)
+            {
+                Assert.AreEqual(inform, testThesis.Inform);
+            }
+            else
+            {
+                Assert.AreNotEqual(inform, testThesis.Inform);
+            }
         }
     }
 }
