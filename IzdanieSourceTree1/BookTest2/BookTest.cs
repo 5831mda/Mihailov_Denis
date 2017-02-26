41ae6579ee7ac017df55d84d2bf5fb3f37c7e5ad
5831mda <denchik_mih@mail.ru>
2017-02-26 19:17:58 +0700

    Добавил [TestCase] для класса Journal

--- IzdanieSourceTree1/BookTest2/BookTest.cs
+++ IzdanieSourceTree1/BookTest2/BookTest.cs
@@ -3,12 +3,15 @@ using IzdanieModel;
 
 namespace BookTestClass
 {
+    [TestFixture]
     public class TestBook
     {
         [Test]
-        public void TestBookInform()
+        [TestCase("sbdfbd", TestName = "при норма вводе")]
+        public void TestBookInform() 
         {
             var testBook = new Book();
+
             testBook.Nameiz = "fairy tales";
             testBook.Surname = "ivanov";
             testBook.Initials = "F.D.";
@@ -17,13 +20,16 @@ namespace BookTestClass
             testBook.City = "Kemerovo";
             testBook.Year = 2017;
             testBook.Page = 234;
-            //testBook.Number = 334;
+            
+            
+              //testBook.Number = 334;
+             
 
-            Assert.AreEqual("ivanov, F.D. DROFA : Phisics / F.D. ivanov. – Kemerovo : fairy tales, 2017. – 234 с. ",
-                testBook.Inform);
+          Assert.AreEqual("ivanov, F.D. DROFA : Phisics / F.D. ivanov. – Kemerovo : fairy tales, 2017. – 234 с. ",
+                 testBook.Inform);
             Assert.AreNotEqual("ivanov, F.D. DROFA : Phisics / F.D. ivanov. – Kemerovo : fairy tales, 2017. – 234с. ",
-                testBook.Inform);
-
+              testBook.Inform);
         }
     }
 }
+
